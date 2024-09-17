910cb3f67bce91b4f6e2c75f71eff107f7d89f6f
abduladilep <111062524+abduladilep@users.noreply.github.com>
2024-09-17 16:16:41 +0530

    2nd commit

--- Strapi/backend/config/plugins.js
+++ Strapi/backend/config/plugins.js
@@ -1,15 +1,24 @@
 // config/plugins.js
-module.exports = {
-    'strapi-plugin-io': {
-      enabled: true,
-      config: {
-        // WebSocket server URL
-        url: 'http://localhost:1337', // Replace with your WebSocket server URL
+// module.exports = {
+//     'strapi-plugin-io': {
+//       enabled: true,
+//       config: {
+//         // WebSocket server URL
+//         url: 'http://localhost:1337', // Replace with your WebSocket server URL
         
-        // Additional configuration options if needed
-        reconnect: true, // Automatically reconnect if the connection is lost
-        // ... other configuration options
-      },
+//         // Additional configuration options if needed
+//         reconnect: true, // Automatically reconnect if the connection is lost
+//         // ... other configuration options
+//       },
+//     },
+//   }
+module.exports = {
+  'strapi-plugin-io': {
+    enabled: true,
+    config: {
+      url: 'http://localhost:1337',
+      reconnect: true,
+      contentTypes: ['your-content-type'], // Ensure you specify content types
     },
-  };
-  
+  },
+};
